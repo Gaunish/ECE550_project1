871aba08a751bad6c67b0b8562cc873049b0f960
Gaunish <anigarg1999@gmail.com>
2021-10-02 15:12:54 -0400

    first

---
+++ Reg-file/regfile_imp.v
@@ -0,0 +1,20 @@
+module regfile_imp(in,
+						 enable,
+						 clk,
+						 clr,
+						 first);
+	 input[31:0] in;
+	input[31:0] enable;
+	input clk;
+	input clr;
+	input first;
+	wire[31:0] out[0:31];
+	 
+	 generate 
+		genvar i;
+			for(i = 0; i < 31; i = i + 1)begin : description
+				register reg1(in, enable[i], out[i], clk, clr, first);
+		end
+	 endgenerate
+
+endmodule
---
+++ Reg-file/register.v
@@ -0,0 +1,19 @@
+module register(input[31:0] d,
+					 input en,
+					 output[31:0] out,
+					 input clk,
+					 input clr,
+					 input first);  
+			
+			wire[31:0] buffer;
+			
+			generate 
+				genvar i;
+				for(i = 0; i < 31; i = i + 1)begin : description
+					dffe_ref DFF1(buffer[i], d[i], clk, en, clr);
+					assign out[i] = first ? buffer[i] : 1'b0;
+				end
+			endgenerate
+					
+			
+endmodule
