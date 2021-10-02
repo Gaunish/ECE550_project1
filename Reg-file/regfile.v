4871e22f61139eb3b2859c62885c544f407c19f7
Julius-1998 <89859253+Julius-1998@users.noreply.github.com>
2021-10-03 01:52:31 +0800

    Modified

--- Reg-file/regfile.v
+++ Reg-file/regfile.v
@@ -12,6 +12,39 @@ module regfile (
 
    output [31:0] data_readRegA, data_readRegB;
 
-   /* YOUR CODE HERE */
+	wire [31:0] decodeA;
+	wire [31:0] decodeB;
+	wire[31:0] out[0:31];
+	
+	//Write Port
+	wire [31:0] decode_write;
+	decoder_5 dWrite(ctrl_writeReg[4:0],decode_write[31:0]);
+	wire[31:0] enable;
+	generate
+		genvar j;
+			for(j = 0;j < 31;j = j +1) begin: write
+				and(enable[j],decode_write[j],ctrl_writeEnable);
+			end
+	endgenerate
+  
+	//register
+	generate 
+		genvar i;
+			register reg0(data_writeReg[4:0], enable[0], out[0], clock, ctrl_reset,1);
+			for(i = 1; i < 31; i = i + 1)begin : description
+			register reg1(data_writeReg[4:0], enable[i], out[i], clock, ctrl_reset,0);
+		end
+	 endgenerate
+	 
+	wire [31:0] decode_readA;
+	wire [31:0] decode_readB;
+
+	decoder_5 dReadA(ctrl_readRegA[4:0],decode_readA[31:0]);
+	decoder_5 dReadB(ctrl_readRegB[4:0],decode_readB[31:0]);
+
+	assign data_readRegA = out[decode_readA[31:0]];
+	assign data_readRegB = out[decode_readB[31:0]];
+
+	
 
 endmodule
