9baa957149be468808923d0697e3dfdcd59d612d
Julius-1998 <89859253+Julius-1998@users.noreply.github.com>
2021-10-03 02:43:15 +0800

    Update regfile.v

--- Reg-file/regfile.v
+++ Reg-file/regfile.v
@@ -3,7 +3,9 @@ module regfile (
     ctrl_writeEnable,
     ctrl_reset, ctrl_writeReg,
     ctrl_readRegA, ctrl_readRegB, data_writeReg,
-    data_readRegA, data_readRegB
+    data_readRegA, data_readRegB,
+	 
+	 outpin
 );
 
    input clock, ctrl_writeEnable, ctrl_reset;
@@ -11,28 +13,29 @@ module regfile (
    input [31:0] data_writeReg;
 
    output [31:0] data_readRegA, data_readRegB;
+	output [31:0] outpin;
 
 	wire [31:0] decodeA;
 	wire [31:0] decodeB;
 	wire[31:0] out[0:31];
 	
 	//Write Port
-	wire [31:0] decode_write;
-	decoder_5 dWrite(ctrl_writeReg[4:0],decode_write[31:0]);
+	wire [31:0] ctrl_decode_write;
+	decoder_5 dWrite(ctrl_writeReg[4:0],ctrl_decode_write[31:0]);
 	wire[31:0] enable;
 	generate
 		genvar j;
 			for(j = 0;j < 31;j = j +1) begin: write
-				and(enable[j],decode_write[j],ctrl_writeEnable);
+				and(enable[j],ctrl_decode_write[j],ctrl_writeEnable);
 			end
 	endgenerate
   
 	//register
 	generate 
 		genvar i;
-			register reg0(data_writeReg[4:0], enable[0], out[0], clock, ctrl_reset,1);
+			register reg0(data_writeReg[31:0], enable[0], out[0], clock, ctrl_reset,1);
 			for(i = 1; i < 31; i = i + 1)begin : description
-			register reg1(data_writeReg[4:0], enable[i], out[i], clock, ctrl_reset,0);
+			register reg1(data_writeReg[31:0], enable[i], out[i], clock, ctrl_reset,0);
 		end
 	 endgenerate
 	 
@@ -42,7 +45,8 @@ module regfile (
 	decoder_5 dReadA(ctrl_readRegA[4:0],decode_readA[31:0]);
 	decoder_5 dReadB(ctrl_readRegB[4:0],decode_readB[31:0]);
 
-	assign data_readRegA = out[decode_readA[31:0]];
+	assign outpin = out[16];
+	assign data_readRegA = out[1];
 	assign data_readRegB = out[decode_readB[31:0]];
 
 	
