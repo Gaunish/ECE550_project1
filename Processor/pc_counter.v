9fa485038493d0556a322a3d3c728760514997f0
Julius-1998 <89859253+Julius-1998@users.noreply.github.com>
2021-10-24 08:43:50 -0400

    Everything

---
+++ Processor/pc_counter.v
@@ -0,0 +1,18 @@
+module pc_counter(clk,rst,pc);
+	input clk; //Input clock
+	input rst; //Input rst
+	
+	//input [11:0] pc;//current pc
+	output reg [11:0] pc;//next pc
+	
+	
+	always@(posedge clk) begin
+		if(rst)
+			pc = 12'h000;
+		else
+			pc = pc + 1;
+	
+		//pc = new_pc;
+	end
+
+endmodule
